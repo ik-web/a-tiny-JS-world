ce789cccdad1ef160e39bdde83c7990446562666
Ihor <vedunigor@gmail.com>
2020-12-08 19:41:27 +0200

    Canged comment about url

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://github.com/ik-web/a-tiny-JS-world
+   Web app: https://ik-web.github.io/a-tiny-JS-world/
 
 */
 
