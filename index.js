d2e09572ecf5bc7e66c8f0e167ed216266da0541
Ihor <vedunigor@gmail.com>
2021-01-02 22:07:41 +0200

    Added method to Inhabitant class for print

--- index.js
+++ index.js
@@ -8,25 +8,32 @@
 
 // ======== OBJECTS DEFINITIONS ========
 class Inhabitant {
-   constructor(species, name, gender, legs, hands, saying) {
+   constructor(species, name, gender, legs, saying) {
       this.species = species;
       this.name = name;
       this.gender = gender;
       this.legs = legs;
-      this.hands = hands;
       this.saying = saying;
+   }
+
+   getInfoToArr() {
+      return [`A ${this.species}`, `(<i>${this.gender}<i>)`, `named <strong>${this.name}</strong>`, `has ${this.legs} legs`, `likes to say <u>${this.saying}</u>`];
+   }
+
+   toString() {
+      return this.getInfoToArr().join(' ');
    }
 }
 
 class Dog extends Inhabitant {
    constructor(name, gender) {
-      super('dog', name, gender, 4, 0, 'woof-woof');
+      super('dog', name, gender, 4, 'woof-woof');
    }
 }
 
 class Cat extends Inhabitant {
    constructor(name, gender) {
-      super('cat', name, gender, 4, 0, 'meow');
+      super('cat', name, gender, 4, 'meow');
    }
 
    getSaying() {
@@ -35,8 +42,9 @@ class Cat extends Inhabitant {
 }
 
 class Human extends Inhabitant {
-   constructor(name, gender, saying) {
-      super('human', name, gender, 2, 2, saying);
+   constructor(name, gender, hands, saying) {
+      super('human', name, gender, 2, saying);
+      this.hands = hands;
    }
 }
 
@@ -48,15 +56,11 @@ class CatWoman extends Inhabitant {
 
 const dog = new Dog('Bobik', 'male');
 const cat = new Cat('Barsik', 'male');
-const man = new Human('Benjamin', 'male', 'Hi to all!');
-const woman = new Human('Sarah', 'female', 'Hello everyone!');
+const man = new Human('Benjamin', 'male', 2, 'Hi to all!');
+const woman = new Human('Sarah', 'female', 2, 'Hello everyone!');
 const catWoman = new CatWoman('anonymos', cat.getSaying());
 const tinyJsWorldPopulation = [dog, cat, man, woman, catWoman];
 
-function toPrint(arr) {
-   arr.forEach( function(obj) {
-      print(`A ${obj.species} (<i>${obj.gender}<i>) named <strong>${obj.name}</strong> has ${obj.legs} legs and ${obj.hands} hands, likes to say <u>${obj.saying}</u>`);
-   });
-}
-
-toPrint(tinyJsWorldPopulation);
+tinyJsWorldPopulation.forEach(function(item) {
+   print(item);
+});
